dac32ed8c45f6d9541705e8f581d3e220c8bf04d
Arda Kurt <to.arda.kurt@gmail.com>
2022-04-08 21:51:35 +0100

    Database Config Add

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const pool = require('./config/db');
+const cors = require('cors');
 const app = express();
-const PORT = 5001;
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({ status: 'On', app: 'Owlster', version: '0.1' });
 });
 
-app.listen(PORT, () => {
-  console.log(`[SERVER]: Listening ${PORT}`);
+require('dotenv').config();
+
+app.listen(proces.env.BACKEND_PORT, () => {
+  console.log(`[SERVER]: Listening ${proces.env.BACKEND_PORT}`);
 });
